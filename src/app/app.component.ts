3c576642cb6ba25d44f694bde549206724c25664
jacekweg <jacekwegr@gmail.com>
2023-10-14 19:48:05 +0200

    experiment 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from './todo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'todo-list';
+export class AppComponent implements OnInit {
+  todos: any[] = [];
+  newTodo = { description: '', summary: '' };
+
+  constructor(private todoService: TodoService) { }
+
+  ngOnInit(): void {
+    this.loadTodos();
+  }
+
+  loadTodos(): void {
+    this.todoService.getTodos().subscribe(todos => this.todos = todos);
+  }
+
+  addTodo(): void {
+    this.todoService.addTodo(this.newTodo).subscribe(() => {
+      this.loadTodos();
+      this.newTodo = { description: '', summary: '' };
+    });
+  }
+
+  deleteTodo(id: number): void {
+    this.todoService.deleteTodo(id).subscribe(() => this.loadTodos());
+  }
 }
